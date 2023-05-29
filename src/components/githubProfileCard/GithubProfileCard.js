71c0f952235c6e7828c060a4017820a7543acba0
ishuar <ishansharma887@gmail.com>
2023-05-30 01:52:53 +0200

    fix: prettier formatting

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./GithubProfileCard.scss";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
-import { contactInfo, isHireable } from "../../portfolio";
+import {contactInfo, isHireable} from "../../portfolio";
 import emoji from "react-easy-emoji";
-import { Fade } from "react-reveal";
+import {Fade} from "react-reveal";
 
-export default function GithubProfileCard({ prof }) {
+export default function GithubProfileCard({prof}) {
   if (isHireable) {
     prof.hireable = "Yes";
   } else {
