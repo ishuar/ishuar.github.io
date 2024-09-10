b7129b69107373b72c75af9aac786d35e8efbfb1
ishuar <ishansharma887@gmail.com>
2024-09-11 01:26:42 +0200

    feat: [books] added component config including header section.
    Signed-off-by: ishuar <ishansharma887@gmail.com>

---
+++ src/components/bookCard/BookCard.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./BookCard.scss";
+
+export default function BookCard({cardInfo, isDark}) {
+  function openUrlInNewTab(url, name) {
+    if (!url) {
+      console.log(`URL for ${name} not found`);
+      return;
+    }
+    var win = window.open(url, "_blank");
+    win.focus();
+  }
+
+  return (
+    <div className={isDark ? "dark-mode book-card" : "book-card"}>
+      <div className="book-image-div">
+        <img
+          src={cardInfo.image}
+          alt={cardInfo.imageAlt || "Card Thumbnail"}
+          className="card-image"
+        ></img>
+      </div>
+      <div className="book-detail-div">
+        <h5 className={isDark ? "dark-mode card-title" : "card-title"}>
+          {cardInfo.title}
+        </h5>
+        <p className={isDark ? "dark-mode card-subtitle" : "card-subtitle"}>
+          {cardInfo.description}
+        </p>
+      </div>
+      <div className="book-card-footer">
+        {cardInfo.footer.map((v, i) => {
+          return (
+            <span
+              key={i}
+              className={
+                isDark ? "dark-mode book-tag" : "book-tag"
+              }
+              onClick={() => openUrlInNewTab(v.url, v.name)}
+            >
+              {v.name}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
